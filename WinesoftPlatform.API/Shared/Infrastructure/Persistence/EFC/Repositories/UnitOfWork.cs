fa175dc2ee8bf7b778bf782f00489d428452d251
GGonzalo <gonzaloacd@outlook.com>
2025-10-26 10:59:17 -0500

    feat(repositories): add base repository and unit of work implementations for data access.

---
+++ WinesoftPlatform.API/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using WinesoftPlatform.API.Shared.Domain.Repositories;
+using WinesoftPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace WinesoftPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+public class UnitOfWork(AppDbContext context) : IUnitOfWork
+{
+    public async Task CompleteAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+}
