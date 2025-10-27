2b3fce409d151072ae9fdb4825d3df26b10bf25d
GGonzalo <gonzaloacd@outlook.com>
2025-10-27 15:51:03 -0500

    feat(supplies): configure supply entity in database context for database mapping.

--- WinesoftPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ WinesoftPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,5 +1,6 @@
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
+using WinesoftPlatform.API.Inventory.Domain.Model.Aggregates;
 using WinesoftPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 
 namespace WinesoftPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
@@ -16,6 +17,13 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+        
+        // Inventory Bounded Context configurations
+        builder.Entity<Supply>().HasKey(s => s.Id);
+        builder.Entity<Supply>().Property(s => s.SupplyName).IsRequired();
+        builder.Entity<Supply>().Property(s => s.Quantity).IsRequired();
+        builder.Entity<Supply>().Property(s => s.Unit).IsRequired();
+        builder.Entity<Supply>().Property(s => s.Price).IsRequired();
         
         // Apply naming convention to use snake_case for database objects
         builder.UseSnakeCaseNamingConvention();
