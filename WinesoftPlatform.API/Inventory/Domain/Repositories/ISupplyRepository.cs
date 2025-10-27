108d45de8fa485b4c851696b93964bcbb4e4ccea
GGonzalo <gonzaloacd@outlook.com>
2025-10-27 15:35:23 -0500

    feat(supplies): add supply repository interface for supply data access.

---
+++ WinesoftPlatform.API/Inventory/Domain/Repositories/ISupplyRepository.cs
@@ -0,0 +1,10 @@
+﻿using WinesoftPlatform.API.Inventory.Domain.Model.Aggregates;
+using WinesoftPlatform.API.Shared.Domain.Repositories;
+
+namespace WinesoftPlatform.API.Inventory.Domain.Repositories;
+
+public interface ISupplyRepository : IBaseRepository<Supply>
+{
+    Task<Supply?> FindByNameAsync(string name);
+    Task<bool> ExistsByNameAsync(string name);
+}
