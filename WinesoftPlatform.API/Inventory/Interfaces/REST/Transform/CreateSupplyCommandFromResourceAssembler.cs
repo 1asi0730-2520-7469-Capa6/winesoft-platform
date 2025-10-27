69d4fafd6a352415219807778078b9a1245a0e05
GGonzalo <gonzaloacd@outlook.com>
2025-10-27 15:47:06 -0500

    feat(supplies): add resource and assembler classes for creating, updating and transforming supplies.

---
+++ WinesoftPlatform.API/Inventory/Interfaces/REST/Transform/CreateSupplyCommandFromResourceAssembler.cs
@@ -0,0 +1,12 @@
+﻿using WinesoftPlatform.API.Inventory.Domain.Model.Commands;
+using WinesoftPlatform.API.Inventory.Interfaces.REST.Resources;
+
+namespace WinesoftPlatform.API.Inventory.Interfaces.REST.Transform;
+
+public static class CreateSupplyCommandFromResourceAssembler
+{
+    public static CreateSupplyCommand ToCommandFromResource(CreateSupplyResource resource)
+    {
+        return new CreateSupplyCommand(resource.SupplyName, resource.Quantity, resource.Unit, resource.Price);
+    }
+}
