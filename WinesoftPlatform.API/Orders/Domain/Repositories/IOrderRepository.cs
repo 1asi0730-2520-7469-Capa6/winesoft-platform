def95d8dc5b011e606661f8e439f3ba64541feb4
Joan Teves <u202117303@upc.edu.pe>
2025-11-02 17:16:15 -0500

    feat(orders): add orders domain and infrastructure layers

---
+++ WinesoftPlatform.API/Orders/Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using WinesoftPlatform.API.Orders.Domain.Model.Aggregates;
+using WinesoftPlatform.API.Shared.Domain.Repositories;
+
+namespace WinesoftPlatform.API.Orders.Domain.Repositories;
+
+public interface IOrderRepository : IBaseRepository<Order>
+{
+
+}
