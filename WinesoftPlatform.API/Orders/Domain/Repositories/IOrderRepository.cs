3efe61d60668e0304cd942f61394357bc59cb674
david <u202516291@upc.edu.pe>
2025-11-03 18:09:05 -0500

    Revert "feat(orders): add orders domain and infrastructure layers"
    This reverts commit def95d8dc5b011e606661f8e439f3ba64541feb4.

--- WinesoftPlatform.API/Orders/Domain/Repositories/IOrderRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using WinesoftPlatform.API.Orders.Domain.Model.Aggregates;
-using WinesoftPlatform.API.Shared.Domain.Repositories;
-
-namespace WinesoftPlatform.API.Orders.Domain.Repositories;
-
-public interface IOrderRepository : IBaseRepository<Order>
-{
-
-}
