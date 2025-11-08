243df1356c7667a47aa30928d18470177c5df0da
Joan Teves <u202117303@upc.edu.pe>
2025-11-08 17:04:08 -0500

    chore(api): configure DI, database, and CORS in Program.cs

--- WinesoftPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ WinesoftPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,11 +1,14 @@
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
+using WinesoftPlatform.API.Orders.Domain.Model.Aggregates;
 using WinesoftPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 
 namespace WinesoftPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
+    public DbSet<Order> Orders { get; set; }
+    
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
         // Automatically set CreatedDate and UpdatedDate for entities
@@ -17,6 +20,16 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     {
         base.OnModelCreating(builder);
         
+        builder.Entity<Order>().ToTable("orders");
+        builder.Entity<Order>().HasKey(o => o.Id);
+        builder.Entity<Order>().Property(o => o.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Order>().Property(o => o.ProductId).IsRequired();
+        builder.Entity<Order>().Property(o => o.Supplier).IsRequired().HasMaxLength(100);
+        builder.Entity<Order>().Property(o => o.Quantity).IsRequired();
+        builder.Entity<Order>().Property(o => o.Status).IsRequired().HasMaxLength(30);
+        builder.Entity<Order>().Property(o => o.CreatedDate);
+        builder.Entity<Order>().Property(o => o.UpdatedDate);
+
         // Apply naming convention to use snake_case for database objects
         builder.UseSnakeCaseNamingConvention();
     }
