759a0e7f29db31c4d7bf7f406353ab80c19bc00a
Joan Teves <u202117303@upc.edu.pe>
2025-11-08 17:37:09 -0500

    fix: configure DI, database, and CORS in Program.cs

--- WinesoftPlatform.API/Program.cs
+++ WinesoftPlatform.API/Program.cs
@@ -1,20 +1,76 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using WinesoftPlatform.API.Shared.Domain.Repositories;
+using WinesoftPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using WinesoftPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using WinesoftPlatform.API.Shared.Infrastructure.Interfaces.ASAP.Configuration;
+using WinesoftPlatform.API.Orders.Domain.Repositories;
+using WinesoftPlatform.API.Orders.Infrastructure.Persistence.EFC.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllPolicy",
+        policy => policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers(options =>
+    options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
 builder.Services.AddOpenApi();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        if (connectionString is null)
+            throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors();
+    });
+}
+else
+{
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        if (connectionString is null)
+            throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        
+        options.UseMySQL(connectionString);
+    });
+}
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.EnsureCreated();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAllPolicy");
 
 app.UseAuthorization();
 
