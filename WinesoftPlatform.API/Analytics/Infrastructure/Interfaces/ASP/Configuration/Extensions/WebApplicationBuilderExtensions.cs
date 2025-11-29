09f715ad80ffe5869725446bef56975f554d5f98
Sltcrd <rodrigo.74.atn@gmail.com>
2025-11-29 01:17:45 -0500

    chore(analytics): register analytics services in DI container

--- WinesoftPlatform.API/Analytics/Infrastructure/Interfaces/ASP/Configuration/Extensions/WebApplicationBuilderExtensions.cs
+++ WinesoftPlatform.API/Analytics/Infrastructure/Interfaces/ASP/Configuration/Extensions/WebApplicationBuilderExtensions.cs
@@ -1,13 +1,24 @@
-
+using WinesoftPlatform.API.Analytics.Application.Internal.CommandServices;
 using WinesoftPlatform.API.Analytics.Application.Internal.QueryServices;
 using WinesoftPlatform.API.Analytics.Domain.Services;
+using WinesoftPlatform.API.Analytics.Infrastructure.Services;
 
 namespace WinesoftPlatform.API.Analytics.Infrastructure.Interfaces.ASP.Configuration.Extensions;
 
+/// <summary>
+/// Extension methods for configuring Analytics context services in a WebApplicationBuilder.
+/// </summary>
 public static class WebApplicationBuilderExtensions
 {
+    /// <summary>
+    /// Adds the Analytics context services to the WebApplicationBuilder.
+    /// </summary>
+    /// <param name="builder">The WebApplicationBuilder to configure.</param>
+
     public static void AddAnalyticsContextServices(this WebApplicationBuilder builder)
     {
         builder.Services.AddScoped<IAnalyticsQueryService, AnalyticsQueryService>();
+        builder.Services.AddScoped<IAnalyticsCommandService, AnalyticsCommandService>();
+        builder.Services.AddScoped<IAnalyticsReportBuilder, QuestPdfAnalyticsReportBuilder>();
     }
 }
