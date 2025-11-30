f26a6c9ed864f25d9244894053ebab4d384ed713
GGonzalo <gonzaloacd@outlook.com>
2025-11-30 10:35:49 -0500

    feat(profiles): add command and query service interfaces for profile management.

---
+++ WinesoftPlatform.API/Profiles/Domain/Services/IProfileQueryService.cs
@@ -0,0 +1,23 @@
+﻿using WinesoftPlatform.API.Profiles.Domain.Model.Aggregates;
+using WinesoftPlatform.API.Profiles.Domain.Model.Queries;
+
+namespace WinesoftPlatform.API.Profiles.Domain.Services;
+
+/// <summary>
+///     Profile query service
+/// </summary>
+public interface IProfileQueryService
+{
+    /// <summary>
+    ///     Handle get all profiles
+    /// </summary>
+    /// <param name="query">
+    ///     The <see cref="GetAllProfilesQuery" /> query
+    /// </param>
+    /// <returns>
+    ///     A list of <see cref="Profile" /> objects
+    /// </returns>
+    Task<IEnumerable<Profile>> Handle(GetAllProfilesQuery query);
+    
+    Task<Profile?> Handle(GetProfileByTaxIdentityQuery query);
+}
