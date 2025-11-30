366dc687658f3dc51c2b6197c7597e708a14f774
GGonzalo <gonzaloacd@outlook.com>
2025-11-30 10:38:01 -0500

    feat(profiles): add resource models for creating and retrieving profiles.

---
+++ WinesoftPlatform.API/Profiles/Interfaces/REST/Resources/CreateProfileResource.cs
@@ -0,0 +1,12 @@
+﻿namespace WinesoftPlatform.API.Profiles.Interfaces.REST.Resources;
+
+public record CreateProfileResource(
+    string BusinessName,
+    string Branch,
+    string Street,
+    string Number,
+    string City,
+    string PostalCode,
+    string Country,
+    string Phone,
+    string LegalId);
